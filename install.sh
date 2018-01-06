d1800f682eb31bc90ab9c50731082479536494aa
thomaswork <xuzhiming_work@163.com>
2018-01-06 20:59:31 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 #不能使用pwd，它返回的是执行命令（的窗口）时所在的路径
 #dirname $0，返回当前执行脚本的父目录
+
+if [ -f ~/.vimrc ]; then
+	if [ -L ~/.vimrc ]; then
+		echo ".vimrc is a symbolic link to a file"
+	else
+		echo ".vimrc is a file"
+	fi
+	mv ~/.vimrc ~/.vimrc.bak
+	echo ".vimrc was renamed to .vimrc.bak"
+fi
+
+if [ -d ~/.vim ]; then
+	if [ -L ~/.vim ]; then
+		echo ".vim is a symbolic link to a folder"
+	else
+		echo ".vim is a folder"
+	fi
+	mv ~/.vim ~/.vim.bak
+	echo ".vim was renamed to .vim.bak"
+fi
+
 bashPath=$(cd `dirname $0`; pwd)
 ln -s $bashPath ~/.vim
 ln -s $bashPath/.vimrc ~/.vimrc
